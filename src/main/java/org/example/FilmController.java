26e7fc36fa19f20ad518dc40990133ef5fa4f69f
FabioGiuriato <fabio.2007.giuriato@gmail.com>
2026-03-04 11:37:45 +0100

    finish

--- src/main/java/org/example/FilmController.java
+++ src/main/java/org/example/FilmController.java
@@ -4,27 +4,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-//per fa comunicare 2 porte diverse
-@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
-    // Lista film in memoria (il nostro "database")
+    // Lista film in memoria "database"
     private List<Film> films = new ArrayList<>(List.of(
             new Film(1L, "Inception",    "Christopher Nolan",   2010),
             new Film(2L, "Il Padrino",   "Francis Ford Coppola",1972),
             new Film(3L, "Interstellar", "Christopher Nolan",   2014)
     ));
 
-    // GET /films → restituisce tutti i film
+    // GET /films restituisce tutti i film
     @GetMapping
     public List<Film> getAll() {
         return films;
     }
 
-    // GET /films/1 → restituisce un film per ID
+    // GET /films/1 restituisce un film per ID
     @GetMapping("/{id}")
     public Film getById(@PathVariable Long id) {
         return films.stream()
@@ -33,7 +31,7 @@ public class FilmController {
                 .orElse(null);
     }
 
-    // POST /films → aggiunge un film
+    // POST /films aggiunge un film
     @PostMapping
     public Film create(@RequestBody Film film) {
         film.setId((long) (films.size() + 1));
@@ -41,7 +39,7 @@ public class FilmController {
         return film;
     }
 
-    // DELETE /films/1 → elimina un film
+    // DELETE /films/1 elimina un film
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Long id) {
         films.removeIf(f -> f.getId().equals(id));
